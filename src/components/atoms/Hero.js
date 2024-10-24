06622077db7235946afc3736f770d4e350fc134d
KshitizSharma11 <kshitizsharma.00@gmail.com>
2024-10-24 08:34:53 +0530

    installed react icons made changes to hero compnent

--- src/components/atoms/Hero.js
+++ src/components/atoms/Hero.js
@@ -1,14 +1,20 @@
 import React from 'react'
-
+import { FaCalendar } from 'react-icons/fa';
 const Hero = () => {
   return (
     <div className='flex flex-row items-center justify-center gap-6 p-4 m-2'>
      <div className='text-gray-100'>
         <h1 className='text-4xl font-medium'>Lorem ipsum dolar ruppes</h1>
         <p className='text-base'>something something something</p>
+        <div className='flex flex-row items-center gap-4 my-4'>
+        <p className='text base'>26th September, 2024</p>
+        <FaCalendar className='text-base'/>
+        </div>
      </div>
-     <div className='flex bg-purple-800 w-96 h-96 rounded-xl items-end'>
-     <img src="https://avatars.githubusercontent.com/u/84636204?v=4" className='rounded-xl w-80- h-80 m-2'/>
+     <div className='flex bg-yellow-400 w-[23rem] h-[23rem] rounded-xl items-end'>
+     <div className='flex bg-purple-800 w-[22rem] h-[22rem] rounded-xl items-end'>   
+     <img src="https://avatars.githubusercontent.com/u/84636204?v=4" className='rounded-xl w-80 h-80 m-2'/>
+     </div>
      </div>
     </div>
   )
